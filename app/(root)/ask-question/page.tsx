8ee24b6e2820faee40940ea4c0d2640d32cb9811
ItsEhsanMM <itsehsanmm@gmail.com>
2023-10-26 12:51:03 +0330

    add assets and layout

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+const Ask = (props: Props) => {
+   return <div>Ask</div>;
+};
+export default Ask;
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-   return (
-      <>
-         <h1 className="h1-bold">this is h1</h1>
-         <h2>this is h2</h2>
-      </>
-   );
-}
