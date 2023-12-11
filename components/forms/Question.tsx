ceadba65cbbc2addef01b80d1dd700f895768c4d
ItsEhsanMM <itsehsanmm@gmail.com>
2023-12-11 22:24:17 +0330

    add question schema

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -19,6 +19,7 @@ import { Input } from "../ui/input";
 import { QuestionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
+import { createQuestion } from "@/lib/actions/Question.action";
 
 const type: any = "create";
 
@@ -37,13 +38,14 @@ const Question = () => {
    });
 
    // 2. Define a submit handler.
-   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+   async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
       setIsSubmitting(true);
 
       try {
          // make an async call to API => create a question
          // contain all form data
          // navigate to home page
+         await createQuestion({});
       } catch (error) {
       } finally {
          setIsSubmitting(false);
@@ -130,6 +132,8 @@ const Question = () => {
                               // @ts-ignore
                               (editorRef.current = editor)
                            }
+                           onBlur={field.onBlur}
+                           onEditorChange={(content) => field.onChange(content)}
                            initialValue=""
                            init={{
                               height: 350,
