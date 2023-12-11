6860c6eaeebb16a3f15017acf9ecfaa88a62affd
ItsEhsanMM <itsehsanmm@gmail.com>
2023-12-11 15:30:16 +0330

    add ask a question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,13 @@
-type Props = {};
-const Ask = (props: Props) => {
-   return <div>Ask</div>;
+import Question from "@/components/forms/Question";
+
+const Ask = () => {
+   return (
+      <div>
+         <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+         <div className="mt-9">
+            <Question />
+         </div>
+      </div>
+   );
 };
 export default Ask;
