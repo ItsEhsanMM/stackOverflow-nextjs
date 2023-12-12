b6630c5991cf97559f38e024f5db963da8523a1d
ItsEhsanMM <itsehsanmm@gmail.com>
2023-12-12 13:19:58 +0330

    add question creation action in mongodb

--- lib/actions/Question.action.ts
+++ lib/actions/Question.action.ts
@@ -1,9 +1,34 @@
 "use server";
 
+import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
 export async function createQuestion(params: any) {
    try {
       connectToDatabase();
+
+      const { title, content, author, tags, path } = params;
+
+      const question = await Question.create({
+         title,
+         content,
+         author,
+      });
+
+      const TagDocument = [];
+
+      for (const tag of tags) {
+         const existedTag = await Tag.findOneAndUpdate(
+            { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+            { $setOnInsert: { name: tag }, $push: { question: question._id } },
+            { upsert: true, new: true }
+         );
+         TagDocument.push(existedTag._id);
+      }
+
+      await Question.findByIdAndUpdate(question._id, {
+         $push: { tags: { $each: TagDocument } },
+      });
    } catch (error) {}
 }
