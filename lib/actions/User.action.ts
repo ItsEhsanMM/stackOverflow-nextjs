054bb5f457ba6fb1fcb967c0f59a6a122716484e
ItsEhsanMM <itsehsanmm@gmail.com>
2023-12-13 15:59:10 +0330

    remove unused variables from user.action.ts

--- lib/actions/User.action.ts
+++ lib/actions/User.action.ts
@@ -67,9 +67,9 @@ export async function deleteUser({ clerkId }: DeleteUserParams) {
          throw new Error(`No user found with the id ${clerkId}`);
       }
 
-      const userQuestionIds = await Question.find({
-         author: user._id,
-      }).distinct("_id");
+      // const userQuestionIds = await Question.find({
+      //    author: user._id,
+      // }).distinct("_id");
 
       await Question.deleteMany({ author: user._id });
 
